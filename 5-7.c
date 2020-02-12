690ea4ba3ef57fa990facd9279a7a0c1ae98d56a
macguff <974123274@qq.com>
2020-02-12 23:55:05 +0800

    add 5-7

---
+++ 5-7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLEN 1000
+#define MAXLINES 5000
+
+int m_getline(char *s, int lim)
+{
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+int readlines(char lines[][MAXLEN], int maxlines)
+{
+    int len, nlines;
+    
+    nlines = 0;
+    
+    while((len = m_getline(lines[nlines], MAXLEN)) > 0){
+        if (nlines > MAXLINES)
+            return -1;
+        else
+            lines[nlines++][len-1] = '\0';
+    }
+    
+    return nlines;
+}
+
